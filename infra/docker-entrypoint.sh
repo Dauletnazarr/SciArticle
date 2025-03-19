697a12c7d7ad81b6339c5a850b130248f5c402bc
FrostWillmott <121764288+FrostWillmott@users.noreply.github.com>
2025-03-19 21:47:33 +0300

    feat:Celery/Redis initial setup

--- infra/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-echo "Fixing ownership of /data for redisuser..."
-chown -R redisuser:redisgroup /data || true
-
-echo "Starting Redis as redisuser..."
-exec su redisuser -c "redis-server --requirepass '$REDIS_PASSWORD'"
