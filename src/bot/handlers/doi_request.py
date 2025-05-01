2cf7bba66af0c76567a6a71c5eb7aa18478e72ef
FrostWillmott <121764288+FrostWillmott@users.noreply.github.com>
2025-05-01 06:30:04 +0300

    fix: some quick fixes, message for repeated DOI request added

--- src/bot/handlers/doi_request.py
+++ src/bot/handlers/doi_request.py
@@ -1,6 +1,7 @@
 import re
 
 from asgiref.sync import sync_to_async
+from django.db import IntegrityError
 from django.utils import timezone
 from telegram import Update
 from telegram.ext import ContextTypes
@@ -27,55 +28,10 @@ def create_request_async(**kwargs):
 
 async def handle_request(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.message
-    # if not msg or not msg.text or not msg.text.startswith("[SciArticle Search]"):
-    #     return
     if not context.args:
         await update.message.reply_text("Пожалуйста, укажите DOI: /request <DOI>")
         return
     doi = context.args[0]
-
-    # doi_match = DOI_PATTERN.search(msg.text)
-    # if not doi_match:
-    #     return
-    # doi = doi_match.group(0)
-    #
-    # chat_user = None
-    # user_id_match = USER_ID_PATTERN.search(msg.text)
-    # if user_id_match:
-    #     target_id = int(user_id_match.group(1))
-    #     chat_user = await fetch_chat_user(target_id)
-    #
-    # if not chat_user:
-    #     requester_id = msg.from_user.id
-    #     username = msg.from_user.username or f"user_{requester_id}"
-    #     full_name = msg.from_user.full_name or ""
-    #     chat_user, _ = await get_or_create_chat_user_async(
-    #         telegram_id=requester_id,
-    #         defaults={
-    #             "username": username,
-    #             "full_name": full_name,
-    #             "is_in_bot": True,
-    #         }
-    #     )
-    #
-    # new_request = await create_request_async(
-    #     doi=doi,
-    #     status="pending",
-    #     chat_id=msg.chat_id,
-    #     request_message_id=msg.message_id,
-    #     user=chat_user,
-    #     created_at=timezone.now(),
-    # )
-    #
-    # await context.bot.send_message(
-    #     chat_id=msg.chat_id,
-    #     reply_to_message_id=msg.message_id,
-    #     text=(
-    #         f"Запрос на DOI `{new_request.doi}` принят.\n"
-    #         "Как только PDF будет доступен — я вас уведомлю."
-    #     ),
-    #     parse_mode="Markdown"
-    # )
     chat_user, _ = await get_or_create_chat_user_async(
         update.effective_user.id,
         defaults={
@@ -85,7 +41,8 @@ async def handle_request(update: Update, context: ContextTypes.DEFAULT_TYPE):
         },
     )
 
-    new_request = await create_request_async(
+    try:
+        new_request = await create_request_async(
         doi=doi,
         status="pending",
         chat_id=update.effective_chat.id,
@@ -93,6 +50,11 @@ async def handle_request(update: Update, context: ContextTypes.DEFAULT_TYPE):
         user=chat_user,
         created_at=timezone.now()
     )
+    except IntegrityError:
+        await update.message.reply_text(
+            "❌ Извините, запрос с таким DOI уже существует. Пожалуйста, введите другой DOI."
+        )
+        return
 
     await update.message.reply_text(
         f"✅ Запрос {doi} принят, жду PDF…",
